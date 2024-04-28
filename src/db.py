2ab5d3870127f689d635216289754969f957f7e8
Alvaro Deras <ad2226@cornell.edu>
2024-04-27 22:11:30 -0400

    Implement Conversation and Message models

--- src/db.py
+++ src/db.py
@@ -8,17 +8,19 @@ db = SQLAlchemy()
 
 class User(db.Model):
     """
-    User Model
+    User model
     """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     # User information
     name = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
     password_digest = db.Column(db.String, nullable=False)
     posts = db.relationship("Post", cascade="delete")
+    conversations = db.relationship("Conversation", cascade="delete")
+    # pfp
 
     # Session information
     session_token = db.Column(db.String, nullable=False, unique=False)
@@ -76,8 +78,10 @@ class User(db.Model):
             "name": self.name,
             "username": self.username,
             "email": self.email,
-            "posts": [p.simple_serialize() for p in self.posts]
+            "posts": [p.simple_serialize() for p in self.posts],
+            "conversations": [c.serialize() for c in self.conversations]
         }
+    
 
 class Post(db.Model):
     """
@@ -85,14 +89,25 @@ class Post(db.Model):
     """
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    description = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    # image
+    item = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=True)
+    location = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __init__(self, **kwargs):
         """
         Initialize a Post object
         """
-        self.description = kwargs.get("description")
+        self.title = kwargs.get("title")
+        self.item = kwargs.get("item")
+        self.status = kwargs.get("status")
+        self.text = kwargs.get("text")
+        self.location = kwargs.get("location")
+        self.timestamp = kwargs.get("timestamp")
         self.user_id = kwargs.get("user_id")
     
     def serialize(self):
@@ -101,6 +116,79 @@ class Post(db.Model):
         """
         return {
             "id": self.id,
-            "description": self.description,
+            "title": self.title,
+            "item": self.item,
+            "status": self.status,
+            "text": self.text,
+            "location": self.location,
+            "timestamp": self.timestamp,
             "user_id": self.user_id
+        }
+
+
+class Conversation(db.Model):
+    """
+    Conversation model
+    """  
+    __tablename__ = "conversations"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user1_messages = []
+    user2_messages = []
+    messages = db.relationship("Message", cascade="delete")
+    user1_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user2_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Conversation object
+        """ 
+        self.user1_id = kwargs.get("user1_id")
+        self.user2_id = kwargs.get("user2_id")
+
+    def serialize(self):
+        """
+        Serialize a Conversation object
+        """
+        return {
+            "id": self.id,
+            "user1_messages": [m.serialize() for m in self.user1_messages],
+            "user2_messages": [m.serialize() for m in self.user2_messages],
+            "user1_id": self.user1_id,
+            "user2_id": self.user2_id
+        }
+    
+
+class Message(db.Model):
+    """
+    Message model
+    """
+    __tablename__ = "messages"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    conversation_id = db.Column(db.Integer, db.ForeignKey("conversations.id"), nullable=False)
+
+    def __init__(self, **kwargs):
+        """
+        Initialize a Message object
+        """
+        self.sender_id = kwargs.get("sender_id")
+        self.receiver_id = kwargs.get("receiver_id")
+        self.content = kwargs.get("content")
+        self.timestamp = kwargs.get("timestamp")
+        self.conversation_id = kwargs.get("conversation_id")
+
+    def serialize(self):
+        """
+        Serialize a Message object
+        """
+        return {
+            "id": self.id,
+            "sender_id": self.sender_id,
+            "receiver_id": self.receiver_id,
+            "content": self.content,
+            "timestamp": self.timestamp,
+            "conversation_id": self.conversation_id
         }
