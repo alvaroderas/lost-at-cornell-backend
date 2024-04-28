b7da23d9fdf6607f2eb3c729987118ebd0ac739c
Alvaro Deras <ad2226@cornell.edu>
2024-04-28 16:17:15 -0400

    Implement User routes

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 import json
-from db import db
+from db import db, User, Post, Conversation, Message
 from flask import Flask, request
 import users_dao
 import datetime
+import bcrypt
 
 app = Flask(__name__)
 db_filename = "lost.db"
@@ -17,7 +18,7 @@ with app.app_context():
 
 def extract_token(request):
     """
-    Extracts the token from a request
+    Extracts the token from request
     """
     auth_header = request.headers.get("Authorization")
     if auth_header is None:
@@ -41,6 +42,8 @@ def failure_response(message, code=404):
     return json.dumps({"error": message}), code
 
 # Routes
+
+# User authentication routes
 
 @app.route("/api/users/register/", methods=["POST"])
 def register_user():
@@ -125,4 +128,83 @@ def renew_session():
         "session_expiration": str(user.session_expiration),
         "refresh_token": user.refresh_token
     })
+
+# User routes
+
+@app.route("/api/users/")
+def get_all_users():
+    """
+    Endpoint for getting all users
+    """
+    return success_response({"users": [u.serialize() for u in User.query.all()]})
+
+@app.route("/api/users/<int:user_id>/")
+def get_user(user_id):
+    """
+    Endpoint for getting a user by id
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    return success_response(user.serialize())
+
+@app.route("/api/users/edit/", methods=["POST"])
+def edit_user():
+    """
+    Endpoint for editing a user's credentials by session token
+    """
+    body = json.loads(request.data)
+    success, response = extract_token(request)
+    if not success:
+        return response
+    session_token = response
+
+    user = users_dao.get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token")
+    
+    name = body.get("name")
+    username = body.get("username")
+    email = body.get("email")
+    password = body.get("password")
+    if name is not None:
+        user.name = name
+    if username is not None:
+        user.username = username
+    if email is not None:
+        user.email = email
+    if password is not None:
+        user.password_digest = bcrypt.hashpw(password.encode("utf8"), bcrypt.gensalt(rounds=13))
+    db.session.commit()
+    return success_response(user.serialize())
+    
+@app.route("/api/users/delete/", methods=["DELETE"])
+def delete_user():
+    """
+    Endpoint for deleting a user by session token
+    """
+    success, response = extract_token(request)
+    if not success:
+        return response
+    session_token = response
+
+    user = users_dao.get_user_by_session_token(session_token)
+    if not user or not user.verify_session_token(session_token):
+        return failure_response("Invalid session token")
+    
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
+
+# Post routes
+
+@app.route("/api/users/<int:user_id>/posts/")
+def get_posts_by_user(user_id):
+    """
+    Endpoint for getting all posts by user by id
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    return success_response({"posts": [p.serialize() for p in user.posts]})
 
