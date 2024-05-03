3c8702448866e285a3c51413583cc31aa11fc760
SerenaInderjit <serenainderjit1@gmail.com>
2024-05-03 18:20:33 -0400

    Add Asset table and image handling

--- src/db.py
+++ src/db.py
@@ -1,10 +1,19 @@
-import datetime
-import hashlib
-import os
-import bcrypt
+import datetime, hashlib, os, base64, boto3, io, random, re, string, bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from mimetypes import guess_extension, guess_type
+from io import BytesIO
+from PIL import Image
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
+
+EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
+BASE_DIR = os.getcwd()
+load_dotenv()
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+AWS_ACCESS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-2.amazonaws.com"
 
 association_table = db.Table(
     "association",
@@ -27,7 +36,7 @@ class User(db.Model):
     password_digest = db.Column(db.String, nullable=False)
     posts = db.relationship("Post", cascade="delete")
     conversations = db.relationship("Conversation", secondary=association_table, back_populates="users")
-    # pfp
+    pfp = db.relationship("Asset", cascade="delete")
 
     # Session information
     session_token = db.Column(db.String, nullable=False, unique=False)
@@ -85,6 +94,7 @@ class User(db.Model):
             "name": self.name,
             "username": self.username,
             "email": self.email,
+            "pfp": self.pfp.serialize(),
             "posts": [p.serialize() for p in self.posts],
             "conversations": [c.serialize() for c in self.conversations]
         }
@@ -97,7 +107,7 @@ class Post(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, nullable=False)
-    # image
+    image = db.relationship("Asset", cascade="delete")
     item = db.Column(db.String, nullable=False)
     status = db.Column(db.String, nullable=False)
     text = db.Column(db.String, nullable=True)
@@ -124,6 +134,7 @@ class Post(db.Model):
         return {
             "id": self.id,
             "title": self.title,
+            "image": self.image.serialize(),
             "item": self.item,
             "status": self.status,
             "text": self.text,
@@ -200,3 +211,93 @@ class Message(db.Model):
             "timestamp": str(self.timestamp),
             "conversation_id": self.conversation_id
         }
+    
+    
+class Asset(db.Model):
+    """
+    Asset model
+    """
+    __tablename__ = "asset"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    base_url = db.Column(db.String, nullable=False)
+    salt = db.Column(db.String, nullable=False)
+    extension = db.Column(db.String, nullable=False)
+    width = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, **kwargs):
+        """
+        Initializes an Asset Object
+        """
+        self.create(kwargs.get("image_data"))
+
+    def serialize(self):
+        """
+        Serializes an Asset Object
+        """
+        return {
+            "url": f"{self.base_url}/{self.salt}.{self.extension}",
+            "created_at": str(self.created_at)
+        }
+
+    def create(self, image_data):
+        """
+        Given an image in base64 encoding, does the following:
+        1. Rejects the image if it is not a supported filename
+        2. Generate a random String for the image filename
+        3. Decodes the image and attempts to upload it to aws
+        """
+        try:
+            ext = guess_extension(guess_type(image_data)[0])[1:]
+            if ext not in EXTENSIONS:
+                raise Exception(f"Extension {ext} is not valid!")
+            
+            salt = "".join(
+                random.SystemRandom().choice(
+                    string.ascii_uppercase + string.digits
+                )
+                for _ in range(16)
+            )
+
+            img_str = re.sub("^data:image/.+;base64,", "", image_data)
+            img_data = base64.b64decode(img_str)
+            img =Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+            self.created_at = datetime.datetime.now()
+
+            img_filename = f"{self.salt}.{self.extension}"
+
+            self.upload(img, img_filename)
+
+
+        except Exception as e:
+            print(f"Error when creating image: {e}")
+
+    def upload(self, img, img_filename):
+        """
+        Attempts to upload the image into the specified S3 bucket
+        """
+        try:
+            #save image into temporary
+            img_temp_loc = f"{BASE_DIR}/{img_filename}"
+            img.save(img_temp_loc)
+            
+            #upload img into S3 bucket
+            s3_client = boto3.client("s3", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+            s3_client.upload_file(img_temp_loc, S3_BUCKET_NAME, img_filename)
+
+            s3_resource = boto3.resource("s3", aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+            object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME, img_filename)
+            object_acl.put(ACL = "public-read")
+            #remove img from temp location
+            os.remove(img_temp_loc)
+
+
+        except Exception as e:
+            print(f"Error when uploading image: {e}")
